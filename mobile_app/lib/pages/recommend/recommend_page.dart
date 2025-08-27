e5c6c8f2583459c3fc6051e81127b13f04a202cc
hyxxyh57 <hyxxyh57@gmail.com>
2025-08-27 14:32:38 +0800

    fix: audio autoplay

--- mobile_app/lib/pages/recommend/recommend_page.dart
+++ mobile_app/lib/pages/recommend/recommend_page.dart
@@ -1,12 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+
 import '../player/video_player_page.dart';
 
 class RecommendPage extends StatelessWidget {
-  const RecommendPage({Key? key}) : super(key: key);
+  final ValueListenable<bool> isActiveListenable;
+  const RecommendPage({Key? key, required this.isActiveListenable})
+    : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // 复用现有的播放页作为发现流承载（本步不改播放逻辑）
-    return const VideoPlayerPage();
+    // 复用现有的播放页作为发现流承载，增加可见性控制
+    return VideoPlayerPage(isActiveListenable: isActiveListenable);
   }
 }
