11d9a87aeb18a169995c815e5b6482208be6751a
hyxxyh57 <hyxxyh57@gmail.com>
2025-08-28 10:11:00 +0800

    chore: unimportant

--- ai_service/test.sh
+++ ai_service/test.sh
@@ -6,6 +6,9 @@
 #   --max-rows 200 \
 #   --test-query "funny time travel"
 
+# 在ai_service下运行
+export PYTHONPATH=$(pwd)/..
+
 
 uvicorn app.main:app --host 0.0.0.0 --port 8088
 curl -H 'Content-Type: application/json' -d '{"question":"test","topK":3,"scene":"search"}'  http://127.0.0.1:8088/rag/ask
